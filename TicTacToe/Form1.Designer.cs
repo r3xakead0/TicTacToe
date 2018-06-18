e578a6f920e51c10ba5b150b63ddb82afb628dbd
Afu Tse <solapa@gmail.com>
2018-06-18 17:15:36 -0500

    Removing all files in .gitignore

--- TicTacToe/Form1.Designer.cs
+++ TicTacToe/Form1.Designer.cs
@@ -46,7 +46,7 @@
             // btn10
             // 
             this.btn10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
-            this.btn10.Location = new System.Drawing.Point(49, 156);
+            this.btn10.Location = new System.Drawing.Point(12, 110);
             this.btn10.Name = "btn10";
             this.btn10.Size = new System.Drawing.Size(75, 75);
             this.btn10.TabIndex = 0;
@@ -57,7 +57,7 @@
             // btn11
             // 
             this.btn11.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
-            this.btn11.Location = new System.Drawing.Point(147, 156);
+            this.btn11.Location = new System.Drawing.Point(110, 110);
             this.btn11.Name = "btn11";
             this.btn11.Size = new System.Drawing.Size(75, 75);
             this.btn11.TabIndex = 0;
@@ -68,7 +68,7 @@
             // btn12
             // 
             this.btn12.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
-            this.btn12.Location = new System.Drawing.Point(247, 156);
+            this.btn12.Location = new System.Drawing.Point(210, 110);
             this.btn12.Name = "btn12";
             this.btn12.Size = new System.Drawing.Size(75, 75);
             this.btn12.TabIndex = 0;
@@ -78,7 +78,7 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(247, 231);
+            this.button5.Location = new System.Drawing.Point(210, 185);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(0, 0);
             this.button5.TabIndex = 0;
@@ -88,7 +88,7 @@
             // btn00
             // 
             this.btn00.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
-            this.btn00.Location = new System.Drawing.Point(49, 58);
+            this.btn00.Location = new System.Drawing.Point(12, 12);
             this.btn00.Name = "btn00";
             this.btn00.Size = new System.Drawing.Size(75, 75);
             this.btn00.TabIndex = 0;
@@ -99,7 +99,7 @@
             // btn01
             // 
             this.btn01.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
-            this.btn01.Location = new System.Drawing.Point(147, 58);
+            this.btn01.Location = new System.Drawing.Point(110, 12);
             this.btn01.Name = "btn01";
             this.btn01.Size = new System.Drawing.Size(75, 75);
             this.btn01.TabIndex = 0;
@@ -110,7 +110,7 @@
             // btn02
             // 
             this.btn02.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn02.Location = new System.Drawing.Point(247, 58);
+            this.btn02.Location = new System.Drawing.Point(210, 12);
             this.btn02.Name = "btn02";
             this.btn02.Size = new System.Drawing.Size(75, 75);
             this.btn02.TabIndex = 0;
@@ -121,7 +121,7 @@
             // btn20
             // 
             this.btn20.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
-            this.btn20.Location = new System.Drawing.Point(49, 257);
+            this.btn20.Location = new System.Drawing.Point(12, 211);
             this.btn20.Name = "btn20";
             this.btn20.Size = new System.Drawing.Size(75, 75);
             this.btn20.TabIndex = 0;
@@ -132,7 +132,7 @@
             // btn21
             // 
             this.btn21.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
-            this.btn21.Location = new System.Drawing.Point(147, 257);
+            this.btn21.Location = new System.Drawing.Point(110, 211);
             this.btn21.Name = "btn21";
             this.btn21.Size = new System.Drawing.Size(75, 75);
             this.btn21.TabIndex = 0;
@@ -143,7 +143,7 @@
             // btn22
             // 
             this.btn22.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
-            this.btn22.Location = new System.Drawing.Point(247, 257);
+            this.btn22.Location = new System.Drawing.Point(210, 211);
             this.btn22.Name = "btn22";
             this.btn22.Size = new System.Drawing.Size(75, 75);
             this.btn22.TabIndex = 0;
@@ -153,7 +153,7 @@
             // 
             // btnPlay
             // 
-            this.btnPlay.Location = new System.Drawing.Point(380, 58);
+            this.btnPlay.Location = new System.Drawing.Point(29, 318);
             this.btnPlay.Name = "btnPlay";
             this.btnPlay.Size = new System.Drawing.Size(75, 23);
             this.btnPlay.TabIndex = 1;
@@ -163,7 +163,7 @@
             // 
             // btnGoBack
             // 
-            this.btnGoBack.Location = new System.Drawing.Point(380, 156);
+            this.btnGoBack.Location = new System.Drawing.Point(110, 318);
             this.btnGoBack.Name = "btnGoBack";
             this.btnGoBack.Size = new System.Drawing.Size(75, 23);
             this.btnGoBack.TabIndex = 3;
@@ -172,7 +172,7 @@
             // 
             // btnGoForward
             // 
-            this.btnGoForward.Location = new System.Drawing.Point(380, 185);
+            this.btnGoForward.Location = new System.Drawing.Point(191, 318);
             this.btnGoForward.Name = "btnGoForward";
             this.btnGoForward.Size = new System.Drawing.Size(75, 23);
             this.btnGoForward.TabIndex = 4;
@@ -183,7 +183,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(572, 521);
+            this.ClientSize = new System.Drawing.Size(301, 357);
             this.Controls.Add(this.btnGoForward);
             this.Controls.Add(this.btnGoBack);
             this.Controls.Add(this.btnPlay);
@@ -197,6 +197,8 @@
             this.Controls.Add(this.btn00);
             this.Controls.Add(this.btn11);
             this.Controls.Add(this.btn10);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "TicTacToe";
             this.ResumeLayout(false);
--- TicTacToe/Properties/AssemblyInfo.cs
+++ TicTacToe/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ticTacToe")]
+[assembly: AssemblyTitle("TicTacToe")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ticTacToe")]
+[assembly: AssemblyProduct("Tic-Tac-Toe")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
